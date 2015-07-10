6ccda10e716d0cde33e8dac59ff526195ed1b21d
demiJade <sy_judy@yahoo.com>
2015-07-10 15:32:24 -0700

    config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-
-  get 'positions/new'
-
-  get 'positions/show'
-
-  get 'positions/edit'
-
-  get 'positions/delete'
+  resources :positions
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
